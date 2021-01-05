96154ad9922d3cbe16c1004156a1ea277d0ec39b
James Hooper <jameshooperdesigns@gmail.com>
2021-01-05 11:48:21 -0700

    Edit the style of the Sushi title

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,15 @@ class Header extends React.Component {
   render() {
     return (
       <header className="top">
-        <h1>Catch 
-          <span className="ofThe">
+        <h1>Sushi
+          {/* <span className="ofThe">
             <span className="of">of</span> 
             <span className="The">The</span>
           </span>
-            Day</h1>
+            Day */}
+            </h1>
         <h3 className="tagline">
-        <span>Fresh Seafood Market</span>
+        <span>Fresh Sushi Market</span>
       </h3>
       </header>
     )
