2af1b3516f7028860935da4b57f1de255d746a66
James Hooper <jameshooperdesigns@gmail.com>
2021-01-07 15:34:20 -0700

    Clean up the code in the index

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import React from 'react';
-// import './index.css';
-// import reportWebVitals from './reportWebVitals';
-// import { render } from '@testing-library/react';
 import { render } from 'react-dom';
 import Router from './components/Router';
 import './css/styles.css';
 
-
-
 render(<Router/>, document.querySelector("#main"));
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     {/* <App /> */}
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
