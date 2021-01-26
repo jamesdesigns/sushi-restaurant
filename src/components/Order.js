890dd465d6d5fc145d513420ac5c3c51259b2584
James Hooper <jameshooperdesigns@gmail.com>
2021-01-26 16:33:31 -0700

    Create a transitionOption variable to minify code

--- src/components/Order.js
+++ src/components/Order.js
@@ -8,15 +8,16 @@ class Order extends React.Component {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
     const isAvailable = fish && fish.status === 'available';
+    const transitionOptions = {
+      classNames: "order", 
+      key, 
+      timeout:{ enter:500, exit:500 }
+    }
     // Make sure the fish is loaded before we continue
     if(!fish) return null;
     if(!isAvailable) {
       return (
-      <CSSTransition 
-      classNames="order" 
-      key={key} 
-      timeout={{ enter:250, exit:250 }}
-      >
+      <CSSTransition {...transitionOptions}>
       <li key={key}>
         Sorry {fish ? fish.name : 'fish'} is no longer available
       </li>
@@ -24,11 +25,7 @@ class Order extends React.Component {
       );
     }
     return (
-    <CSSTransition 
-      classNames="order" 
-      key={key} 
-      timeout={{ enter:250, exit:250 }}
-    >
+    <CSSTransition {...transitionOptions}>
       <li key={key}>
         <span>
           <TransitionGroup component="span" className="count">
