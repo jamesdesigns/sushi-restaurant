87a1842f96adf27ce7f60a43cfcc94e8b56c1ebb
James Hooper <jameshooperdesigns@gmail.com>
2021-01-26 17:08:45 -0700

    Add all PropTypes throught each component that has props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // Stateless Functional Component
 const Header = props => (
@@ -9,5 +10,9 @@ const Header = props => (
 </h3>
 </header>
 );
+
+Header.propTypes = {
+  tagline: PropTypes.string.isRequired
+}
 
 export default Header;
