f90c5d21fc0b077b4b137ed99ae73d0db3646b2a
James Hooper <jameshooperdesigns@gmail.com>
2021-01-27 16:41:33 -0700

    Add Login information for the Inventory component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Login = () => (
+const Login = (props) => (
   <nav className="login">
     <h2>Inventory Login</h2>
     <p>Sign in  to manage your store's inventory.</p>
-    <button className="github" onClick={() => this.props.authenticate('Github')}></button>
+    <button 
+      className="github" 
+      onClick={() => props.authenticate('Github')}
+    >
+      Login with GitHub
+    </button>
   </nav>
 );
+
+Login.propTypes = {
+  authenticate: PropTypes.func.isRequired
+}
 
 export default Login;
