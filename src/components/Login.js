fb6536cf40115eea5486e45724ffe1db8564e163
James Hooper <jameshooperdesigns@gmail.com>
2021-01-27 14:53:07 -0700

    Add nav to Login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Login = () => (
-
+  <nav className="login">
+    <h2>Inventory Login</h2>
+    <p>Sign in  to manage your store's inventory.</p>
+    <button className="github" onClick={() => this.props.authenticate('Github')}></button>
+  </nav>
 );
 
 export default Login;
