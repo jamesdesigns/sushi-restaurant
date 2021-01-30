fd231bad295114976de8079837c209aa14f6f3fe
James Hooper <jameshooperdesigns@gmail.com>
2021-01-30 11:51:29 -0700

    Remove header title

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 // Stateless Functional Component
 const Header = props => (
   <header className="top">
-  {/* <h1>Sushi</h1> */}
   <img src="/images/sushi-logo.png" alt="Sushi Logo" width="269" height="184" /><br />
   <h3 className="tagline">
     <span>{props.tagline}</span>
