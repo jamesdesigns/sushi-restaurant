effea763017ff94c7ce1d7072841b94efdcfe289
James Hooper <jameshooperdesigns@gmail.com>
2021-02-01 11:49:42 -0700

    Edit the image paths

--- src/sample-fishes.js
+++ src/sample-fishes.js
@@ -2,7 +2,7 @@
 const fishes = {
   fish1: {
     name: "Pacific Halibut",
-    image: "/images/sushi-a.jpeg",
+    image: "/images/sushi-a.jpg",
     desc:
       "Everyone’s favorite white fish. We will cut it to the size you need and ship it.",
     price: 1724,
@@ -11,7 +11,7 @@ const fishes = {
 
   fish2: {
     name: "Lobster",
-    image: "/images/sushi-b.jpeg",
+    image: "/images/sushi-b.jpg",
     desc:
       "These tender, mouth-watering beauties are a fantastic hit at any dinner party.",
     price: 3200,
@@ -20,7 +20,7 @@ const fishes = {
 
   fish3: {
     name: "Sea Scallops",
-    image: "/images/sushi-c.jpeg",
+    image: "/images/sushi-c.jpg",
     desc:
       "Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 8-10 per pound",
     price: 1684,
@@ -29,7 +29,7 @@ const fishes = {
 
   fish4: {
     name: "Mahi Mahi",
-    image: "/images/sushi-d.jpeg",
+    image: "/images/sushi-d.jpg",
     desc:
       "Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes. ",
     price: 1129,
@@ -38,7 +38,7 @@ const fishes = {
 
   fish5: {
     name: "King Crab",
-    image: "/images/sushi-e.jpeg",
+    image: "/images/sushi-e.jpg",
     desc:
       "Crack these open and enjoy them plain or with one of our cocktail sauces",
     price: 4234,
@@ -47,7 +47,7 @@ const fishes = {
 
   fish6: {
     name: "Atlantic Salmon",
-    image: "/images/sushi-f.jpeg",
+    image: "/images/sushi-f.jpg",
     desc:
       "This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!",
     price: 1453,
@@ -56,7 +56,7 @@ const fishes = {
 
   fish7: {
     name: "Oysters",
-    image: "/images/sushi-g.jpeg",
+    image: "/images/sushi-g.jpg",
     desc: "A soft plump oyster with a sweet salty flavor and a clean finish.",
     price: 2543,
     status: "available"
@@ -64,7 +64,7 @@ const fishes = {
 
   fish8: {
     name: "Mussels",
-    image: "/images/sushi-h.jpeg",
+    image: "/images/sushi-h.jpg",
     desc:
       "The best mussels from the Pacific Northwest with a full-flavored and complex taste.",
     price: 425,
@@ -73,7 +73,7 @@ const fishes = {
 
   fish9: {
     name: "Jumbo Prawns",
-    image: "/images/sushi-i.jpeg",
+    image: "/images/sushi-i.jpg",
     desc:
       "With 21-25 two bite prawns in each pound, these sweet morsels are perfect for shish-kabobs.",
     price: 2250,
