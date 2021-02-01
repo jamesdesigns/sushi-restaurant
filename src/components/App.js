50d0da1e0c3ad3a4322dbeeaf7ec6b24047fec43
James Hooper <jameshooperdesigns@gmail.com>
2021-02-01 15:09:37 -0700

    Edit styles of content

--- src/components/App.js
+++ src/components/App.js
@@ -95,7 +95,9 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="catch-of-the-day">
+      <div
+        className="catch-of-the-day"
+        >
         <div className="menu">
           <Header tagline="MENU" />
           <ul className="fishes">
